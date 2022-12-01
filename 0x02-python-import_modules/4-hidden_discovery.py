0114e52836c042da5611019d4f128bd572440e4a
ermitse <ermiastse@gmail.com>
2022-12-01 13:07:51 -0800

    all

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 
     import hidden_4
 
-    def_names = dir(hidden_4)
-    for name in def_names:
+    defined_names = dir(hidden_4)
+    for name in defined_names:
         if name[:2] != "__":
             print(name)
