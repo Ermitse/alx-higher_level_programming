9fed1d0851900062f21d86e54be004b04268cdc1
ermitse <ermiastse@gmail.com>
2022-12-09 10:06:31 -0800

    chmod

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 def complex_delete(a_dictionary, value):
-    keys_to_del = []
-    for key in a_dictionary:
-        if a_dictionary[key] == value:
-            keys_to_del.append(key)
-    for key in keys_to_del:
-        del a_dictionary[key]
-    return 
+    tmp = a_dictionary.copy()
+    for k, v in tmp.items():
+        if value == v:
+            a_dictionary.pop(k)
+    return a_dictionary
